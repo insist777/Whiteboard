70ade5d7e480562a99207b87248529f0ab1d6a05
jikefan <1791504346@qq.com>
2022-11-01 00:28:20 +0800

    初始化前端项目 #!

---
+++ front-end/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://v2.vuejs.org/">Vue 2</a>.
+    </h3>
+    <h3>
+      Member:
+      <a target="_blank" href="https://github.com/insist777">insist777</a> &
+      <a target="_blank" href="https://github.com/jikefan">jikefan</a> &
+      <a target="_blank" href="https://github.com/Arrbole">Arrbole</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  top: -10px;
+  font-size: 2.6rem;
+  font-weight: 500;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    display: block;
+    text-align: left;
+  }
+}
+</style>
