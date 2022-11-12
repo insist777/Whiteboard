e05eb9669d1fe4b7fec255abd75c6705420c0c5d
Drunk <11437348+arrebolle@user.noreply.gitee.com>
2022-11-12 16:38:07 +0800

    fix: 顶部菜单、侧边栏拆分

---
+++ front-end/src/views/whiteBoard/components/asideMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="elContainer w-12 mt-12">
+      <div class="iconf my-2">
+        <div><i class="iconfont icon-ArrowUpRight text-xl px-2"></i></div>
+        <div><i class="iconfont icon-pencil text-xl px-2"></i></div>
+        <div><i class="iconfont icon-line text-xl font-bold px-2"></i></div>
+        <div><i class="iconfont icon-circle text-xl px-2"></i></div>
+        <div>
+          <i class="iconfont icon-triangle-s-o text-xl px-2 text-gray-500"></i>
+        </div>
+        <div><i class="iconfont icon-Rectangle text-xl px-2"></i></div>
+        <div><i class="iconfont icon-tuoyuan text-2xl px-2"></i></div>
+        <div><i class="iconfont icon-text text-2xl px-2 text-gray-500"></i></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AsideMenu',
+};
+</script>
+
+<style lang="less" scoped>
+.elContainer {
+  .iconf {
+    div {
+      margin-top: 12px;
+      margin-bottom: 12px;
+    }
+  }
+}
+</style>
--- front-end/src/views/whiteBoard/index.vue
+++ front-end/src/views/whiteBoard/index.vue
@@ -1,63 +1,15 @@
 <template>
   <div class="h-screen w-screen">
-    <el-container class="h-screen">
+    <el-container :key="index" class="h-screen" @click="clickedTool">
       <el-header
-        class="bg-gray-100 fixed bg-transparent top-0 left-0 right-0 z-20 shadow-md leading-10"
+        class="bg-gray-100 fixed bg-transparent top-0 left-0 right-0 z-20 leading-10 shadow-sm"
         height="40px"
       >
-        <div>
-          <i class="iconfont icon-fanhui cursor-pointer hover:bg-gray-200 p-1"></i
-          ><span class="px-5">|</span>
-          <i class="iconfont icon-Undo cursor-pointer hover:bg-gray-200 p-1"></i>
-          <i class="iconfont icon-Redo cursor-pointer mx-5 hover:bg-gray-200 p-1"></i>
-          <span class="pr-5">|</span>
-          <el-select v-model="fontSelect" class="font">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            >
-            </el-option>
-          </el-select>
-          <span class="pr-5">|</span>
-          <el-input-number
-            v-model="fontSize"
-            class="count"
-            controls-position="right"
-            :min="1"
-            :max="10"
-            size="small"
-          ></el-input-number>
-          <span class="px-5">|</span>
-          <i class="iconfont icon-tianse cursor-pointer text-gray-500 hover:bg-gray-200 p-1"></i>
-          <i class="el-icon-edit cursor-pointer ml-3 hover:bg-gray-200 p-1"></i>
-          <i class="iconfont icon-image cursor-pointer hover:bg-gray-200 p-1 ml-3"></i>
-          <span class="px-5">|</span>
-          <i class="iconfont icon-select cursor-pointer text-gray-800 hover:bg-gray-200 p-1"></i>
-          <i class="iconfont icon-cut cursor-pointer hover:bg-gray-200 p-1 mx-3"></i>
-          <i class="iconfont icon-copy cursor-pointer mr-5 hover:bg-gray-200 p-1"></i>
-          <i class="iconfont icon-paste cursor-pointer text-gray-500 hover:bg-gray-200 p-1"></i>
-          <span class="px-5">|</span>
-          <i class="iconfont icon-clear cursor-pointer text-gray-800 hover:bg-gray-200 p-1"></i>
-        </div>
+        <HeaderMenu />
       </el-header>
-      <el-container class="mt-10 text-center">
-        <div class="w-12 bg-gray-100 h-screen">
-          <div class="my-2">
-            <div class="my-3"><i class="iconfont icon-ArrowUpRight text-xl px-2"></i></div>
-            <div class="my-3"><i class="iconfont icon-pencil text-xl px-2"></i></div>
-            <div class="my-3"><i class="iconfont icon-line text-xl font-bold px-2"></i></div>
-            <div class="my-3"><i class="iconfont icon-circle text-xl px-2"></i></div>
-            <div class="my-3">
-              <i class="iconfont icon-triangle-s-o text-xl px-2 text-gray-500"></i>
-            </div>
-            <div class="my-3"><i class="iconfont icon-Rectangle text-xl px-2"></i></div>
-            <div class="my-3"><i class="iconfont icon-tuoyuan text-2xl px-2"></i></div>
-            <div class="my-3"><i class="iconfont icon-text text-2xl px-2 text-gray-500"></i></div>
-          </div>
-        </div>
-        <el-main class="bg-white">
+      <el-container class="text-center h-screen bg-gray-100">
+        <AsideMenu />
+        <el-main class="bg-white mt-10">
           <div></div>
         </el-main>
       </el-container>
@@ -66,49 +18,26 @@
 </template>
 
 <script>
+import { Tools } from './config';
+import HeaderMenu from './components/headerMenu.vue';
+import AsideMenu from './components/asideMenu.vue';
+
 export default {
+  components: {
+    HeaderMenu,
+    AsideMenu,
+  },
   data() {
     return {
-      options: [
-        {
-          value: '选项1',
-          label: '微软雅黑',
-        },
-        {
-          value: '选项2',
-          label: '宋体',
-        },
-        {
-          value: '选项3',
-          label: '楷体',
-        },
-      ],
-      fontSelect: '选项1',
-      fontSize: 1,
+      toolNames: Object.values(Tools),
     };
+  },
+  methods: {
+    clickedTool(data) {
+      console.log(data);
+    },
   },
 };
 </script>
 
-<style scoped lang="less">
-.count ::v-deep.el-input {
-  width: 50px;
-}
-::v-deep .el-input__inner {
-  padding: 0;
-  border: 0;
-  background-color: transparent;
-}
-::v-deep .el-input-number__decrease,
-.el-input-number__increase {
-  padding: 0;
-  border: 0;
-  background-color: transparent;
-}
-.font ::v-deep .el-input__inner {
-  width: 100px;
-  padding: 0;
-  border: 0;
-  background-color: transparent;
-}
-</style>
+<style scoped lang="less"></style>
