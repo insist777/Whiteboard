8ee8f4b67b7a855088915a8ccb966c3dfc9014df
Drunk <11437348+arrebolle@user.noreply.gitee.com>
2022-11-13 01:34:08 +0800

    fix: 获取鼠标坐标

--- front-end/src/views/whiteBoard/components/asideMenu.vue
+++ front-end/src/views/whiteBoard/components/asideMenu.vue
@@ -2,16 +2,22 @@
   <div>
     <div class="elContainer w-12 mt-12">
       <div class="iconf my-2">
-        <div><i class="iconfont icon-ArrowUpRight text-xl px-2"></i></div>
-        <div><i class="iconfont icon-pencil text-xl px-2"></i></div>
-        <div><i class="iconfont icon-line text-xl font-bold px-2"></i></div>
-        <div><i class="iconfont icon-circle text-xl px-2"></i></div>
-        <div>
-          <i class="iconfont icon-triangle-s-o text-xl px-2 text-gray-500"></i>
+        <div id="arrow">
+          <i class="iconfont icon-ArrowUpRight text-xl p-2 hover:bg-gray-200"></i>
         </div>
-        <div><i class="iconfont icon-Rectangle text-xl px-2"></i></div>
-        <div><i class="iconfont icon-tuoyuan text-2xl px-2"></i></div>
-        <div><i class="iconfont icon-text text-2xl px-2 text-gray-500"></i></div>
+        <div id="pen"><i class="iconfont icon-pencil text-xl p-2 hover:bg-gray-200"></i></div>
+        <div id=""><i class="iconfont icon-line text-xl font-bold p-2 hover:bg-gray-200"></i></div>
+        <div id="circle"><i class="iconfont icon-circle text-xl p-2 hover:bg-gray-200"></i></div>
+        <div id="triangle">
+          <i class="iconfont icon-triangle-s-o text-xl p-2 hover:bg-gray-200 text-gray-500"></i>
+        </div>
+        <div @click="clickRect">
+          <i class="iconfont icon-Rectangle text-xl p-2 hover:bg-gray-200"></i>
+        </div>
+        <div id="elli"><i class="iconfont icon-tuoyuan text-2xl p-2 hover:bg-gray-200"></i></div>
+        <div id="text">
+          <i class="iconfont icon-text text-2xl p-2 hover:bg-gray-200 text-gray-500"></i>
+        </div>
       </div>
     </div>
   </div>
@@ -20,6 +26,17 @@
 <script>
 export default {
   name: 'AsideMenu',
+  data() {
+    return {
+      iconIsclick: '',
+    };
+  },
+  methods: {
+    clickRect() {
+      this.iconIsclick = 'rect';
+      this.$emit('clickData', this.iconIsclick);
+    },
+  },
 };
 </script>
 
